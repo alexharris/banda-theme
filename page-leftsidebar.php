2f63cc3c0a7c4bc7dcf7b0e6aa0875128d7f37f7
Bill Hogan <hogan.wp@gmail.com>
2016-06-18 09:49:15 -0700

    left sidebar

--- page-leftsidebar.php
+++ page-leftsidebar.php
@@ -1,11 +1,6 @@
 <?php 
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template name: Page, left sidebar
  *
  */
 
@@ -13,33 +8,27 @@ get_header(); ?>
 <div class="container container-normal">
    <div class="row maincontent"><div class="col-sm-12"><?php the_breadcrumb(); ?></div></div>
       <div class="row maincontent lower">
-        <div class="col-sm-8">
+       
         <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+        
+        <?php get_sidebar(); ?>
+        
+        <div class="col-sm-8">
+            <div class="post">
+                <h2 class="page-title"><?php the_title(); ?></h2>
+                <div class="entry">
+                    <?php the_content(); ?>
+                </div>
+            </div> <!-- /post -->
          
-        <div class="post">
-        <h2 class="page-title"><?php the_title(); ?></h2>
+        <?php endwhile; ?>
  
- 
-            <div class="entry">
-            <?php the_content(); ?>
-
- 
+            <div class="navigation">
+                <?php posts_nav_link(); ?>
             </div>
- 
-        </div>
-         
-<?php endwhile; ?>
 
+        </div><!-- col-sm-8 -->
+        <?php endif; ?>
 
- 
-    <div class="navigation">
-        <?php posts_nav_link(); ?>
-    </div>
- 
-<?php endif; ?>
-    </div><!-- col-sm-8 -->
-  <!--/div>
-
-<!-- row // will be closed in sidebar -->
-<?php get_sidebar(); ?>   
+   
 <?php get_footer(); ?>
