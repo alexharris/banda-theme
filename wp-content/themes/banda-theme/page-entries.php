c38e522193e29f923723695729b831824d0d584b
alexharris <alexharris6@gmail.com>
2016-08-08 21:09:44 -0700

    add card for custom post fields to entry view

--- wp-content/themes/banda-theme/page-entries.php
+++ wp-content/themes/banda-theme/page-entries.php
@@ -47,7 +47,40 @@ get_header(); ?>
                                             <h3><?php the_title(); ?></h3>
 
                                             <?php the_content(); ?>
+
                                             </div>
+                                            <?php
+                                                if(count(get_post_custom()) > 2) { ?>
+                                                    <div class="card">
+                                                        <div class="card-header">
+                                                          Custom Fields
+                                                        </div>
+                                                        <div class="card-block">
+
+                                                            <dl class="row">
+                                                                <?php
+                                                                    // get custom fields
+                                                                    $custom_field_keys = get_post_custom();
+                                                                    //go through them
+                                                                    foreach ( $custom_field_keys as $key => $value ) {
+
+                                                                        //check the first character to make sure these are not wordpress default "_edit" custom fields
+
+                                                                        $valuet = trim($key);
+                                                                        if ( '_' == $valuet{0} )
+                                                                            continue;
+                                                                ?>
+                                                                <dt class="col-sm-3"><?php echo $key; ?></dt>
+                                                                <dd class="col-sm-9"><?php echo $value[0]; ?></dd>
+                                                                <?php  } ?>
+                                                            </dl>
+                                                      </div>
+                                                    </div>
+                                                </div>
+                                                <?php 
+                                                };
+                                            ?>
+
                                             <hr />
                                               <?php
                                           }
