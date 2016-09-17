89150bb035d011c5cb732806f1f4de1695e4a029
wphogan <hogan.wp@gmail.com>
2016-09-17 12:41:25 -0700

    add bs class to search field

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
+
   <label>
     <span class="screen-reader-text hidden">Search for:</span>
-    <input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:" />
+    <input type="search" class="search-field form-control" placeholder="Search …" value="" name="s" title="Search for:" />
   </label>
-  <input type="submit" class="search-submit hidden" value="Search" />
+  <input type="submit" class="search-submit hidden btn-primary" value="Search" />
 </form>
